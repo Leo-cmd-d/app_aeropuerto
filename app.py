f15178c2becbef1158b00e41787df37af39f52ce
Leo-cmd-d <leonotega4@gmail.com>
2026-03-17 11:13:10 -0500

    version4

--- app.py
+++ app.py
@@ -67,11 +67,11 @@ with st.container(border=True):
 
     col1, col2, col3 =st.columns(3)
     with col1:
-        st.metric('Mínimo',f'{minimo:.0f}',border=True)
+        st.metric('Mínimo',f'{minimo:.0f}','100',border=True)
     with col2:
-        st.metric('Media',f'{media:.0f}',border=True)
+        st.metric('Media',f'{media:.0f}','-10%',border=True)
     with col3:
-        st.metric('Máximo',f'{maximo:.0f}',border=True)
+        st.metric('Máximo',f'{maximo:.0f}','15%',border=True)
 
     #VER DATAFRAME EN DESPLEGABLE:
 with st.expander('Ver Matriz de Datos'):
